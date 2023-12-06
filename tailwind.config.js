cf21ff9c711d8f0951a846d595f59e09b09ae4f3
farell <rhezkyfarell@gmail.com>
2023-12-06 11:18:39 +0700

    now very small phone cna scroll it

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      'sm': '450px',
+      'sm': '282px',
       'md': '650px',
       'lg': '1185px',
     },
